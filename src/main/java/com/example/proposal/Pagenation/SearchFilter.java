8ec64493d5d07ad95785108fa4f71774e53d5b36
RishabhFyntune <you@example.com>
2025-04-08 10:28:37 +0530

    Added Filtering

---
+++ src/main/java/com/example/proposal/Pagenation/SearchFilter.java
@@ -0,0 +1,41 @@
+package com.example.proposal.Pagenation;
+
+public class SearchFilter
+{
+    private String name;
+    private String email;
+    private String city;
+    private Character status;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Character getStatus() {
+        return status;
+    }
+
+    public void setStatus(Character status) {
+        this.status = status;
+    }
+}
